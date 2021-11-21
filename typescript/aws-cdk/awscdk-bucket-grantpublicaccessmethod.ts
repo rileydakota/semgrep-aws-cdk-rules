a02d905c0fe49ade24c2d49034938d02ee52edae
Dakota Riley <dakota@Dakotas-MBP.localdomain>
2021-11-20 21:22:59 -0500

    feat: add bucket grantpublicaccess rule

---
+++ typescript/aws-cdk/awscdk-bucket-grantpublicaccessmethod.ts
@@ -0,0 +1,22 @@
+import * as cdk from '@aws-cdk/core';
+import * as codebuild from '@aws-cdk/aws-codebuild';
+import * as s3 from '@aws-cdk/aws-s3';
+// import * as sqs from '@aws-cdk/aws-sqs';
+
+export class CdkStarterStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // ruleid:awscdk-bucket-grantpublicaccessmethod
+    const publicBucket1 = new s3.Bucket(this, 'bucket')
+    console.log('something unrelated')
+    publicBucket1.grantPublicAccess()
+
+    // ruleid:awscdk-bucket-grantpublicaccessmethod
+    const publicBucket2 = new s3.Bucket(this, 'bucket')
+    publicBucket2.grantPublicAccess()
+
+    // ok:awscdk-bucket-grantpublicaccessmethod
+    const nonPublicBucket = new s3.Bucket(this, 'bucket')
+  }
+}
