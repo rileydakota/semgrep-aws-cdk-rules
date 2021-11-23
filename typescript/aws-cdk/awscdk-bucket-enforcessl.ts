af729a843ff9c8537d1ccd5ce3ccc89ac1d64a8a
rileydakota <dakotariley2@gmail.com>
2021-11-23 10:05:29 -0500

    feat: add support for direct import for enforcessl rule

--- typescript/aws-cdk/awscdk-bucket-enforcessl.ts
+++ typescript/aws-cdk/awscdk-bucket-enforcessl.ts
@@ -1,6 +1,7 @@
 import * as s3 from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 import * as rename_s3 from '@aws-cdk/aws-s3';
+import {Bucket} from '@aws-cdk/aws-s3';
 
 export class CdkStarterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -26,5 +27,16 @@ export class CdkStarterStack extends cdk.Stack {
     const badBucket2Renamed = new rename_s3.Bucket(this, 's3-bucket-bad', {
       enforceSSL: false
      })
+
+     // ruleid:aws-cdk-bucket-enforcessl
+     const badBucketDirect = new Bucket(this, 's3-bucket-bad')
+     // ok:aws-cdk-bucket-enforcessl
+     const AnotherGoodBucketDirect = new Bucket(this, 's3-bucket', {
+       enforceSSL: true
+     })
+     // ruleid:aws-cdk-bucket-enforcessl
+     const badBucket2Direct = new Bucket(this, 's3-bucket-bad', {
+       enforceSSL: false
+      })
   }
 }
